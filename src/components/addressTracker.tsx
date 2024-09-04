6909ced3aecd1a97d23fd1f32dea2594d5d5c5f0
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-04 17:18:39 +0100

    enh: upgrade the design style

--- src/components/addressTracker.tsx
+++ src/components/addressTracker.tsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { useState } from "react";
+import { FaAngleRight } from "react-icons/fa";
 
 interface addressType {
-    ip: string;
-    country: string;
-    city: string;
-    timezone: string;
+  ip: string;
+  country: string;
+  city: string;
+  timezone: string;
 }
 
 const AddressTracker = () => {
@@ -19,29 +20,52 @@ const AddressTracker = () => {
 
   return (
     <div>
-      <h1>Track IP ADDRESS</h1>
-      <input
-        type="string"
-        placeholder="type the address you want to track"
-        className="border-2 border-black rounded-md p-2 w-1/2"
-        onChange={(e) => setIpAddress(e.target.value)}
-      />
-      <button
-        onClick={() => fetchAddress(ipAddress)}
-        className="bg-blue-700 text-white rounded-md p-2 m-2"
-      >
-        Search
-      </button>
-      <div>
+      <div className="bg-pattern h-[30vh] justify-center items-center m-auto flex flex-col">
+        <h1 className="text-3xl m-5 font-bold  text-white">
+          IP Address Tracker
+        </h1>
+        <div className="w-[600px] justify-center flex shadow-lg rounded-md">
+          <input
+            type="string"
+            placeholder="Search for any IP address or domain"
+            className="p-2 pl-4 w-full rounded-l-md"
+            onChange={(e) => setIpAddress(e.target.value)}
+          />
+          <button
+            onClick={() => fetchAddress(ipAddress)}
+            className="bg-black text-white rounded-r-md p-2"
+          >
+            <FaAngleRight />
+          </button>
+        </div>
+
+        <div className="flex justify-center items-center m-auto shadow-xl  bg-white rounded-lg w-[80vh] h-32">
         {address && (
-            <>
-            <p>ip address: {address.ip}</p>
-            <p>Country: {address.country}</p>
-            <p>City: {address.city}</p>
-            <p>timezone: {address.timezone}</p>
+          <>
+            <p className="flex flex-col p-5 pr-8">
+              <span className="text-md text-gray-400">ip address</span>{" "}
+              <span className="font-bold text-2xl">{address.ip}</span>
+            </p>
+            <span className="h-16 w-0.5 bg-gray-400"></span>
+            <p className="flex flex-col p-5 pr-8">
+              <span className="text-md text-gray-400">Country</span>{" "}
+              <span className="font-bold text-2xl">{address.country}</span>
+            </p>
+            <span className="h-16 w-0.5 bg-gray-400"></span>
+            <p className="flex flex-col p-5 pr-8">
+              <span className="text-md text-gray-400">City</span>{" "}
+              <span className="font-bold text-2xl">{address.city}</span>
+            </p>
+            <span className="h-16 w-0.5 bg-gray-400"></span>
+            <p className="flex flex-col p-5 pr-8">
+              <span className="text-md text-gray-400">timezone</span>{" "}
+              <span className="font-bold text-2xl">{address.timezone}</span>
+            </p>
           </>
         )}
       </div>
+      </div>
+      
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'pattern': "url('./src/assets/pattern-bg-desktop.png')",
+      }
+    },
   },
   plugins: [],
 }
