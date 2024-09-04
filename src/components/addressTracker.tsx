e2b0828a22e29f70cb6364c8ff4b9c609b1032ce
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-04 16:08:14 +0100

    track ip address by search, define the typing , show simple data without styling

--- src/components/addressTracker.tsx
+++ src/components/addressTracker.tsx
@@ -1,25 +1,54 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const AddressTracker = () => {
-    const [address, setAddress] = useState('')
-
-    useEffect(() => {
-        const fetchAddress = async () => {
-            const response = await axios.get("https://api.ipify.org?format=json", {
-                // params: {
-                //     access_key: import.meta.env.IP_API_KEY,
-                // },
-            });
-            setAddress(response.data.ip);
-        };
-        fetchAddress()
-    }, []);
-
-    console.log("Address: ", address)
-  return (
-    <div>hello this where you the address</div>
-  )
+interface addressType {
+    ip: string;
+    country: string;
+    city: string;
+    timezone: string;
 }
 
-export default AddressTracker
+const AddressTracker = () => {
+  const [address, setAddress] = useState<addressType | null>(null);
+  const [ipAddress, setIpAddress] = useState("");
+
+  const fetchAddress = async (ipAddress: string) => {
+    const response = await axios.get(`https://ipinfo.io/${ipAddress}/geo`);
+    setAddress(response.data);
+  };
+
+  useEffect(() => {
+    fetchAddress(ipAddress);
+  }, [ipAddress]);
+
+  console.log("Address: ", address);
+  return (
+    <div>
+      <h1>Track IP ADDRESS</h1>
+      <input
+        type="string"
+        placeholder="type the address you want to track"
+        className="border-2 border-black rounded-md p-2 w-1/2"
+        onChange={(e) => setIpAddress(e.target.value)}
+      />
+      <button
+        onClick={() => fetchAddress(ipAddress)}
+        className="bg-blue-700 text-white rounded-md p-2 m-2"
+      >
+        Search
+      </button>
+      <div>
+        {address && (
+            <>
+            <p>ip address: {address.ip}</p>
+            <p>Country: {address.country}</p>
+            <p>City: {address.city}</p>
+            <p>timezone: {address.timezone}</p>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AddressTracker;
