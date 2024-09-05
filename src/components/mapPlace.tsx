0a32fdd5511ea65bbaeb5e2c02d768aab1f44fa9
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-05 14:05:03 +0100

    fix map square problem

--- src/components/mapPlace.tsx
+++ src/components/mapPlace.tsx
@@ -1,26 +1,34 @@
+import { LatLngExpression } from 'leaflet';
 import React from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import "leaflet/dist/leaflet.css"
 
 interface MapPlaceProps {
-    lat:string
-    lng:string
-    center: string[]
+    lat?:string
+    lng?:string
 }
 
 const MapPlace:React.FC<MapPlaceProps> = ({ lat, lng }) => {
-    const position = [lat, lng];
+    const position : LatLngExpression= [Number(lat), Number(lng)];
+    console.log("position : ",position)
   return (
-    <div className='border-2 border-black'>
-    <MapContainer center={position} zoom={13} style={{ height: "100%", width: "100%" }}>
+    <div style={{
+        height:"500px",
+        width:"500px"
+    }}>
+    <MapContainer center={{
+        lat : Number(lat),
+        lng : Number(lng)
+    }} zoom={13} style={{ height: "700px", width: "700px" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <Marker position={{ lat: Number(lat), lng: Number(lng) }}>
+          <Popup>
+            This is your selected position: {lat}, {lng}
+          </Popup>
+        </Marker>
     </MapContainer>
     </div>
   )
