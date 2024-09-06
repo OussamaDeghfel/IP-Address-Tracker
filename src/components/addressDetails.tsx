1aa409913b73dad87101171a25c4aa872bc7cdcd
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-06 18:20:04 +0100

    refactor

--- src/components/addressDetails.tsx
+++ src/components/addressDetails.tsx
@@ -1,38 +1,36 @@
-import React from 'react'
-import { addressType } from './addressTracker'
+import React from "react";
+import { addressType } from "./addressTracker";
 
 interface addressDetailsType {
-    address: addressType | null
-  }
-
-const AddressDetails:React.FC<addressDetailsType> = ({address}) => {
-  return (
-    <>
-        {address && (
-          <>
-            <p className="flex flex-col p-5 pr-8">
-              <span className="text-md text-gray-400">ip address</span>{" "}
-              <span className="font-bold text-2xl">{address.ip}</span>
-            </p>
-            <span className="hidden lg:block lg:h-16 lg:w-0.5 lg:bg-gray-400"></span>
-            <p className="flex flex-col p-5 pr-8">
-              <span className="text-md text-gray-400">Country</span>{" "}
-              <span className="font-bold text-2xl">{address.country}</span>
-            </p>
-            <span className="hidden lg:block lg:h-16 lg:w-0.5 lg:bg-gray-400"></span>
-            <p className="flex flex-col p-5 pr-8">
-              <span className="text-md text-gray-400">City</span>{" "}
-              <span className="font-bold text-2xl">{address.city}</span>
-            </p>
-            <span className="hidden lg:block lg:h-16 lg:w-0.5 lg:bg-gray-400"></span>
-            <p className="flex flex-col p-5 pr-8">
-              <span className="text-md text-gray-400">timezone</span>{" "}
-              <span className="font-bold text-2xl">{address.timezone}</span>
-            </p>
-          </>
-        )}
-    </>
-  )
+  address: addressType | null;
 }
 
-export default AddressDetails
+const AddressDetails: React.FC<addressDetailsType> = ({ address }) => {
+  return (
+    <>
+    <div className="z-10 flex flex-col bg-white mt-16 text-center shadow-2xl rounded-3xl justify-center items-center m-auto">
+      <p className="flex flex-col p-5 pr-8">
+        <span className="text-md text-gray-400">ip address</span>{" "}
+        <span className="font-bold text-2xl">{address?.ip}</span>
+      </p>
+      <span className="hidden lg:block lg:h-16 lg:w-0.5 lg:bg-gray-400"></span>
+      <p className="flex flex-col p-5 pr-8">
+        <span className="text-md text-gray-400">Country</span>{" "}
+        <span className="font-bold text-2xl">{address?.country}</span>
+      </p>
+      <span className="hidden lg:block lg:h-16 lg:w-0.5 lg:bg-gray-400"></span>
+      <p className="flex flex-col p-5 pr-8">
+        <span className="text-md text-gray-400">City</span>{" "}
+        <span className="font-bold text-2xl">{address?.city}</span>
+      </p>
+      <span className="hidden lg:block lg:h-16 lg:w-0.5 lg:bg-gray-400"></span>
+      <p className="flex flex-col p-5 pr-8">
+        <span className="text-md text-gray-400">timezone</span>{" "}
+        <span className="font-bold text-2xl">{address?.timezone}</span>
+      </p>
+      </div>
+    </>
+  );
+};
+
+export default AddressDetails;
